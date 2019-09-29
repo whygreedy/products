1e35ec357c143a48a06e96fe7eb4b914a7b2fa04
whygreedy <cindyyeah9@gmail.com>
2019-09-29 20:58:47 +0800

    add header

--- products.py
+++ products.py
@@ -11,6 +11,7 @@ for p in products:
     print(p[0], '的價格是', p[1])
 
 # 將清單存為檔案
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
