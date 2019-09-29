2c96b393ccda12f2bb130217e8b4111ed9dbb0ff
whygreedy <cindyyeah9@gmail.com>
2019-09-29 17:55:27 +0800

    add writing file

--- products.py
+++ products.py
@@ -6,7 +6,11 @@ while True:
 		break
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
-print(products)
 
 for p in products:
-	print(p[0], '的價格是', p[1])
+    print(p[0], '的價格是', p[1])
+
+# 將清單存為檔案
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
