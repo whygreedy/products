fc15d1133d09b509685dfea1fb9f0cd78543cf54
whygreedy <cindyyeah9@gmail.com>
2019-09-29 21:26:52 +0800

    add read file

--- products.py
+++ products.py
@@ -1,5 +1,15 @@
 
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 繼續, 跳到下一回
+		name, price = line.strip().split (',')
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -7,10 +17,11 @@ while True:
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
-# 將清單存為檔案
+# 將清單存為檔案和加上標題欄位
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
