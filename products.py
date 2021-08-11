14ca55b00b32cb03dd8dbda54fa7a3c88f0e6f60
whygreedy <55575130+whygreedy@users.noreply.github.com>
2021-08-11 16:57:23 +0800

    refactor_v2

--- products.py
+++ products.py
@@ -1,52 +1,49 @@
-import os # operating system
+import os
 
-# 讀取檔案
 def read_file(filename):
 	products = []
-	with open(filename, 'r', encoding='utf-8') as f: # 檢查檔案在不在 
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
-				continue # 繼續, 跳到下一回
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	return products	
+				continue
+			else:
+				name, price = line.strip().split(',')
+				products.append([name, price])
+	return products
 
 
-# 讓使用者輸入
+def print_product(products):
+	print(products)
+
+
 def user_input(products):
 	while True:
-		name = input('請輸入商品名稱: ')
+		name = input('商品: ')
 		if name == 'q':
 			break
-		price = input('請輸入商品價格: ')
+		price = input('價格: ')
 		products.append([name, price])
 	return products
 
 
-# 印出所有購買紀錄
-def print_products(products):
-	for p in products:
-	    print(p[0], '的價格是', p[1])
-
-
-# 將清單存為檔案和加上標題欄位
 def write_file(filename, products):
 	with open(filename, 'w', encoding='utf-8') as f:
 		f.write('商品,價格\n')
 		for p in products:
 			f.write(p[0] + ',' + p[1] + '\n')
-	
+
 
 def main():
+	products = []
 	filename = 'products.csv'
 	if os.path.isfile(filename):
-		print('Yeah! Find the file!')
+		print('檔案存在')
+		products = read_file(filename)
+		print_product(products)
 	else:
-		print('File cannot be found.....')
-
-	products = read_file(filename)
+		print('檔案不存在')
 	products = user_input(products)
-	print_products(products)
-	write_file('products.csv', products)
+	write_file(filename, products)
+
 
 main()
